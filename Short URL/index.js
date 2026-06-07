bdff611a5b2b2c3ae2e905b7dacd3a4140beadd5
iamkrishpathak <pathak0404@gmail.com>
2026-06-07 22:47:20 +0530

    Server Side Rendering Part - 1

--- Short URL/index.js
+++ Short URL/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const {connectToDb} = require("./connection");
+const path = require("path");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const { url } = require("inspector");
 
 const app = express();
 const PORT = 8001;
@@ -10,14 +12,25 @@ connectToDb("mongodb://localhost:27017/short-url").then(() =>
     console.log("MongoDB Connected Successfully!")
 );
 
+//telling server that yes it's ejs, also locating it
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+
+app.get("/test", async (req,res) => {
+    const allUrls = await URL.find({});
+    return res.render("home",{
+        urls : allUrls
+    });
+});
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req,res) => {
+app.get("/url/:shortId", async (req,res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
-        shortID : shortId
+        shortId : shortId
     },
     {
         $push: {
